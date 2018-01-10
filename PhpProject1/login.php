434783ceeadcf40077c610f624a6d3c40f1f16ad
LaMaSi <lara@sievers-home.de>
2018-01-10 15:54:31 +0100

    Css und so

--- PhpProject1/login.php
+++ PhpProject1/login.php
@@ -19,9 +19,10 @@
             <input type="password" name="password" placeholder="Passwort">
   	</div>
   	<div class="input-group">
-  		<button type="submit" class="btn" name="login_user">Login</button>
+  		<button type="submit" class="btn" id="login" name="login_user">Login</button>
+		<button type="submit" class="btn" id="guest" name="guest_user">Gastzugang</button>
   	</div>
-  	<p>
+  	<p class="registerParagraph">
   		Noch kein Mitglied? <a href="register.php">Registrieren</a>
   	</p>
   </form>
