16503b2cd3ee045cbc8dbc11902ec9463f53e28c
LaMaSi <lara@sievers-home.de>
2018-01-24 14:36:59 +0100

    Tab Control im login

--- PhpProject1/login.php
+++ PhpProject1/login.php
@@ -1,30 +1,73 @@
 <?php include('server.php') ?>
 <!DOCTYPE html>
 <html>
-<head>
-  <title>ChitCat</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-  <div class="header">
-  	<h2>Anmelden</h2>
-  </div>
-	 
-  <form method="post" action="login.php">
-  	<?php include('errors.php'); ?>
-  	<div class="input-group">
-  		<input type="text" name="username" placeholder="Benutzername">
-  	</div>
-  	<div class="input-group">
-            <input type="password" name="password" placeholder="Passwort">
-  	</div>
-  	<div class="input-group">
-  		<button type="submit" class="btn" id="login" name="login_user">Login</button>
-		<button type="submit" class="btn" id="guest" name="guest_user">Gastzugang</button>
-  	</div>
-  	<p class="registerParagraph">
-  		Noch kein Mitglied? <a href="register.php">Registrieren</a>
-  	</p>
-  </form>
-</body>
+	<head>
+	  <title>ChitCat</title>
+	  <link rel="stylesheet" type="text/css" href="styleKopie.css">
+	</head>
+	
+	<body>
+	<script src="tabControl.js"></script>
+	  <div class="tab">
+		<button class="tablinks" onclick="openForm(event, 'Register')">Register</button>
+		<button class="tablinks" onclick="openForm(event, 'Login')" id="defaultOpen">Login</button>
+		<button class="tablinks" onclick="openForm(event, 'Gastzugang')">Gastzugang</button>
+	  </div>
+	  
+	  	<div id="Register" class="tabcontent">
+			<form method="post" action="register.php">
+				<?php include('errors.php'); ?>
+				<div class="input-group">
+					  <input type="text" name="username" placeholder="Benutzername" value="<?php echo $username; ?>">
+				</div>
+				<div class="input-group">
+				  <input type="email" name="email" placeholder="E-Mail" value="<?php echo $email; ?>">
+				</div>
+				<div class="input-group">
+				  <input type="password" name="password_1" placeholder="Passwort">
+				</div>
+				<div class="input-group">
+				  <input type="password" name="password_2" placeholder="Passwort bestätigen">
+				</div>
+				<div class="input-group">
+				  <button type="submit" class="btn" name="reg_user">Registrieren</button>
+				</div>
+			</form>
+		</div>
+	  
+		<div id="Login" class="tabcontent">
+			<form method="post" action="login.php">
+				<?php include('errors.php'); ?>
+				<div class="input-group">
+					<input type="text" name="username" placeholder="Benutzername">
+				</div>
+				<div class="input-group">
+					<input type="password" name="password" placeholder="Passwort">
+				</div>
+				<div class="input-group">
+					<button type="submit" class="btn" id="login" name="login_user">Login</button>
+				</div>	
+			</form>
+		</div>
+		
+		<div id="Gastzugang" class="tabcontent">
+		<form method="post" action="ChatWindow.php">
+			<?php include('errors.php'); ?>
+			<div class="input-group">
+				<input type="text" name="username" placeholder="Benutzername">
+			</div>
+			<p class="registerParagraph">
+				Achtung: Für Gäste werden keine Daten gespeichert.
+			</p>
+			<div class="input-group">
+				<button type="submit" class="btn" id="login" name="login_user">Zum Chat</button>
+			</div>
+			</form>
+		</div>
+		
+		<script>
+			document.getElementById("defaultOpen").click();
+		</script>
+		
+	</body>
 </html>
